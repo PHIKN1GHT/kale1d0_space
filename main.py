b9481bc52833220a23117f5bc2957b2de70135e9
PHIN1GHT <kale1d0@qq.com>
2021-02-13 20:09:47 +0800

    add_auto_update

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+import asyncio, httpx, subprocess
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="weblog", html=True))
 
+async def auto_pull():
+    subprocess.Popen(["git", "pull"], cwd="weblog")
+    repo_url = 'https://api.github.com/repos/PHIKN1GHT/phikn1ght.github.io'
+    async with httpx.AsyncClient() as client:
+        last_upd = (await client.get(repo_url)).json()['updated_at']
+        print('LAST UPDATE: {}'.format(last_upd))
+        while True:
+            await asyncio.sleep(60)
+            updtime = (await client.get(repo_url)).json()['updated_at']
+            if not updtime == last_upd:
+                print('NEW UPDATE: {}'.format(updtime))
+                subprocess.Popen(["git", "pull"], cwd="weblog")
+                last_upd = updtime
+
+@app.on_event("startup")
+async def onStartup():
+    asyncio.create_task(auto_pull())
+
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(loop="asyncio", app='__main__:app', host='0.0.0.0', port=80)
-
+    uvicorn.run(loop="asyncio", app="__main__:app", host='0.0.0.0', port=8000, log_level='debug')
